ffbbf3baab30590a1702c6af255b5407e01bda56
henriquejunqueira <henriquejunqueira15@gmail.com>
2026-06-07 18:46:56 -0300

    Finalizado token service, criada verificação de token

--- src/main/java/com/henriquejunqueira/login_auth_api/infra/security/TokenService.java
+++ src/main/java/com/henriquejunqueira/login_auth_api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package com.henriquejunqueira.login_auth_api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.henriquejunqueira.login_auth_api.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -24,6 +25,16 @@ public class TokenService {
             return token;
         }catch(JWTCreationException exception){
             throw new RuntimeException("Erro de autenticação...");
+        }
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm).withIssuer("login-auth-api").build().verify(token).getSubject();
+        }catch (JWTVerificationException exception){
+            return null;
         }
     }
 
