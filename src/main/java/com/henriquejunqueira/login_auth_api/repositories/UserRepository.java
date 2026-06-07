d94cc514c5a52904afc069b7a3cb9d602021fe2e
henriquejunqueira <henriquejunqueira15@gmail.com>
2026-06-07 19:09:01 -0300

    Criado filtro de segurança

--- src/main/java/com/henriquejunqueira/login_auth_api/repositories/UserRepository.java
+++ src/main/java/com/henriquejunqueira/login_auth_api/repositories/UserRepository.java
@@ -3,6 +3,9 @@ package com.henriquejunqueira.login_auth_api.repositories;
 import com.henriquejunqueira.login_auth_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
 
+    Optional<User> findByEmail(String email);
 }
